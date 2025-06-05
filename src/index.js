8e052f98b614590b9e440f2d0a769113887bc1a7
nublius <184036331+nublius@users.noreply.github.com>
2025-06-04 23:06:41 -0700

    landing page

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 import "./styles.css";
+
+import restaurantPic from "./images/restaurant.jpg";
+
+const coverImg = document.createElement("img");
+coverImg.src = restaurantPic;
+
+const imgContainer = document.querySelector(".img__container");
+imgContainer.append(coverImg);
+
 console.log("Hello, World!");
